bfed5171375b4cc2dcb186c3f26eda76e82c5a2e
Eli Herrera <esrbastianherrera@gmail.com>
2024-12-23 12:36:12 -0600

    feat: make that only one time have to type the password

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Disable sudo password prompt
+echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
+
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+
 source $PROJECT_PATH/apps/shell/shell.sh
 
 source $PROJECT_PATH/apps/kitty/kitty.sh
+
+# Revert to normal idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
+
+# Remove the sudoers file to revert the sudo configuration
+sudo rm /etc/sudoers.d/$USER
